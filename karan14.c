61749cd41bb9b108b7cca75cf7d7888d3c8dbabc
Arikaranmm <35688917+Arikaranmm@users.noreply.github.com>
2018-02-05 13:38:04 +0530

    Create karan14.c

---
+++ karan14.c
@@ -0,0 +1,14 @@
+#include <stdio.h>  
+int main() 
+{
+int counter; 
+printf("Even numbers between 1 to 50\n"); 
+for(counter = 1; counter <= 50; counter++)
+     {   
+      if(counter%2 == 0)
+      { 
+      printf("%d ", counter);  
+      }  
+    }  
+   return 0;  
+} 
