265c52d6f66066cf95418685b109dca49add1729
Arikaranmm <35688917+Arikaranmm@users.noreply.github.com>
2018-02-21 14:14:17 +0530

    Create karan40.c

---
+++ karan40.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,i;
+char[50]="mechanical";
+printf("How many times \n");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+printf("%s \n",a);
+}
+return 0;
+}
