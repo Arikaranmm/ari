b001801ad29f15615cf6f907af51834d697f6718
Arikaranmm <35688917+Arikaranmm@users.noreply.github.com>
2018-02-26 14:16:31 +0530

    product num odd or even

---
+++ karan54.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter the numbers \n");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    if(c%2==0)
+    {
+        printf("number is odd \n");
+    }
+    else
+    {
+        printf("number is even \n");
+}
+}
