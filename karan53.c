ed3fbef9551afea3ddb0d065074bfbd8be967c7d
Arikaranmm <35688917+Arikaranmm@users.noreply.github.com>
2018-02-26 14:00:38 +0530

    Create karan53.c

---
+++ karan53.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int i;
+printf("Enter the number\n");
+scanf("%d",i);
+if(i%2==0)
+{
+printf("Nearest number %d",i-2);
+}
+else
+printf("Nearest number %d,i-1);
+}
