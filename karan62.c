ddf6e0cd80dc8b15fe5759252548a8d29be93411
Arikaranmm <35688917+Arikaranmm@users.noreply.github.com>
2018-03-07 14:21:16 +0530

    Update karan62.c

--- karan62.c
+++ karan62.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include<string.h>
-int main(void) {
-	char a[25];
-  int word=1,l;
-  printf("Enter the string");
-  scanf("%d",a);
-	l=strlen(a);
-	for(int i=0;i<=l;i++)
-	{
-		if(a[i]==' ')
-		++word;
-	}
-	printf("The no of word in a sentence is %d",word);
-	return 0;
+int main()
+{
+char a[25];
+int word=1,l;
+printf("Enter the string");
+scanf("%d",a);
+l=strlen(a);
+for(int i=0;i<=l;i++)
+{
+if(a[i]==' ')
+++word;
+}
+printf("The no of word in a sentence is %d",word);
+return 0;
 }
