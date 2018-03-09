9504112e03cb0236c42e65879cadfeedc2476b33
Arikaranmm <35688917+Arikaranmm@users.noreply.github.com>
2018-03-09 16:17:35 +0530

    multiple by n

---
+++ karan69.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a;
+    printf("Enter the values \n");
+    scanf("%d",&a);
+    if(a%7==0)
+    {
+        printf("Yes");
+    }
+    else    
+    {
+    printf("No");
+    }
+  return 0;
+}
+ 
