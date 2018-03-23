3922b0286ac7f94483535a4a1704137fe27f5a82
Arikaranmm <35688917+Arikaranmm@users.noreply.github.com>
2018-03-23 16:01:02 +0530

    Create karan88.c

---
+++ karan88.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,min,l;
+scanf("%d%d",&a,&b);
+min=(a<b)?a:b;
+for(min=2; min <= a && min <= b; ++min)
+{
+if((a%min==0)&&(b%min==0))
+{
+  break;
+}
+}
+l=(a*b)/min;
+printf("LCM of given number is = %d",l);
+getch();
+}
